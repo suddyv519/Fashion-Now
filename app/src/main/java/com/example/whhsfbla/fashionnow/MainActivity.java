8cbb227d2bf28c9a7d5a7b30f5d2e3eb54ddfcfb
suddyv519 <venkatesh.sudharshan@gmail.com>
2015-12-28 21:42:09 -0500

    parse integration

--- app/src/main/java/com/example/whhsfbla/fashionnow/MainActivity.java
+++ app/src/main/java/com/example/whhsfbla/fashionnow/MainActivity.java
@@ -5,12 +5,27 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/android/guide#local-datastore
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this);
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+
     }
 
     @Override
