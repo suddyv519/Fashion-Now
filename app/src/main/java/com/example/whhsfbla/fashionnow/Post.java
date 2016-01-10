152b0da0dfb44f6dae4401a48e452dd7f16293f2
suddyv519 <venkatesh.sudharshan@gmail.com>
2016-01-10 15:14:46 -0500

    Feed working with preloaded Parse entries

---
+++ app/src/main/java/com/example/whhsfbla/fashionnow/Post.java
@@ -0,0 +1,12 @@
+package com.example.whhsfbla.fashionnow;
+
+/**
+ * Created by Sudharshan on 1/10/2016.
+ */
+public class Post {
+    protected String username, picURL;
+
+    public Post() {
+
+    }
+}
