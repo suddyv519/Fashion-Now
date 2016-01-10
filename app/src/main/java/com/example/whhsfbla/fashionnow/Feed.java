401c05f9628b48cf6cff792b50ab6e79f61ee0a9
suddyv519 <venkatesh.sudharshan@gmail.com>
2016-01-09 21:47:34 -0500

    cardview started

--- app/src/main/java/com/example/whhsfbla/fashionnow/Feed.java
+++ app/src/main/java/com/example/whhsfbla/fashionnow/Feed.java
@@ -2,14 +2,25 @@ package com.example.whhsfbla.fashionnow;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class Feed extends Activity {
+
+    RecyclerView recList;
+    SwipeRefreshLayout swipeRefreshLayout;
+    LinearLayoutManager lin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        RecyclerView drawerRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        drawerRecyclerView.setHasFixedSize(true);
+
     }
 
 }
